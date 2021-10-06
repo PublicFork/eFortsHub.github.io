7e890447554d18063bb786c11ceba96e26c65c2c
H. Bappi <53597251+hbappi@users.noreply.github.com>
2021-10-06 23:14:34 +0600

    Add files via upload

---
+++ public-html/a.java
@@ -0,0 +1,13 @@
+public class a{
+    public static void main(String[] args){
+        
+        for(int i=90; i<300; i++){
+            String s = "https://api.quran.com/api/v4/quran/translations/"+i;
+
+            System.out.println(s);
+
+        }
+
+    }
+
+}
